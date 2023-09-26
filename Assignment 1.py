2e3e3a26e29dfdd039b615b837267f2284586be5
Adi Bintang Pradana <78474342+adibintangprada@users.noreply.github.com>
2023-09-26 15:53:32 +0700

    deleting credentials

--- Assignment 1.py
+++ Assignment 1.py
@@ -2,13 +2,6 @@
 from google.cloud import storage
 import urllib.request
 
-# Defining variables
-project_id = 'data-fellowship-7-363107'
-bucket_name = 'data_fellowship7'
-source_file_name = 'https://ae01.alicdn.com/kf/He5af0c200eb345188c44345d7d649875Q.jpg_640x640Q90.jpg'
-destination_blob_name = 'gundam_exia.jpg'
-storage_client = storage.Client.from_service_account_json('credentials.json')
-
 # Defining function for upload the file
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     file = urllib.request.Request(source_file_name, headers={'User-Agent': 'Mozilla/5.0'})   
